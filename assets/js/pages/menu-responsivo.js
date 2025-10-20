0b9e931c93a4fd67dac9443dabfe2d9a5d45047c
batistadaniel <batistanegobiz@gmail.com>
2025-10-20 02:09:17 -0300

    teste

---
+++ assets/js/pages/menu-responsivo.js
@@ -0,0 +1,21 @@
+const menuBtn = document.getElementById('btn-menu-header');
+const sideMenu = document.getElementById('menu-lateral');
+const overlay = document.getElementById('overlay');
+
+menuBtn.addEventListener('click', () => {
+  sideMenu.classList.toggle('open');
+  overlay.classList.toggle('show');
+  document.body.classList.toggle('menu-open'); // impede rolagem do fundo
+});
+
+overlay.addEventListener('click', closeMenu);
+
+document.querySelectorAll('#menu-lateral a').forEach(link => {
+  link.addEventListener('click', closeMenu);
+});
+
+function closeMenu() {
+  sideMenu.classList.remove('open');
+  overlay.classList.remove('show');
+  document.body.classList.remove('menu-open');
+}
