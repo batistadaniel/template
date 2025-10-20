5ffbd7da1f7af34029591eea259bcf2fdc533334
batistadaniel <batistanegobiz@gmail.com>
2025-10-20 15:33:34 -0300

    teste 6

--- assets/js/pages/menu-responsivo.js
+++ assets/js/pages/menu-responsivo.js
@@ -3,11 +3,26 @@ const sideMenu = document.getElementById('menu-lateral');
 const overlay = document.getElementById('overlay');
 
 menuBtn.addEventListener('click', () => {
-	sideMenu.classList.toggle('open');
+	const isOpen = sideMenu.classList.toggle('open');
 	overlay.classList.toggle('show');
-	document.body.classList.toggle('menu-open'); // impede rolagem do fundo nao funciona
+	document.body.classList.toggle('menu-open', isOpen); // impede rolagem do fundo. nao funciona
+
+	// alterna o ícone
+	menuBtn.textContent = isOpen ? 'X' : '☰';
 });
 
+
+
+// const iconeBtnHeader = menuBtn.textContent;
+
+// if (iconeBtnHeader == 'X') {
+// 	menuBtn.addEventListener('click', () => {
+// 		menuBtn.innerHTML = '☰';
+// 	});
+// }
+// console.log(iconeBtnHeader)
+
+// quando clicar na parte de fora tbm vai fechar o menu
 overlay.addEventListener('click', closeMenu);
 
 document.querySelectorAll('#menu-lateral a').forEach(link => {
@@ -18,4 +33,5 @@ function closeMenu() {
 	sideMenu.classList.remove('open');
 	overlay.classList.remove('show');
 	document.body.classList.remove('menu-open');
+	menuBtn.textContent = '☰';
 }
