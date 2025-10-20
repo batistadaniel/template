294f899ab17ad3c4b5716491565d0294c068f178
batistadaniel <batistanegobiz@gmail.com>
2025-10-20 11:23:52 -0300

    teste 3

--- assets/js/pages/menu-responsivo.js
+++ assets/js/pages/menu-responsivo.js
@@ -3,19 +3,19 @@ const sideMenu = document.getElementById('menu-lateral');
 const overlay = document.getElementById('overlay');
 
 menuBtn.addEventListener('click', () => {
-  sideMenu.classList.toggle('open');
-  overlay.classList.toggle('show');
-  document.body.classList.toggle('menu-open'); // impede rolagem do fundo
+	sideMenu.classList.toggle('open');
+	overlay.classList.toggle('show');
+	document.body.classList.toggle('menu-open'); // impede rolagem do fundo nao funciona
 });
 
 overlay.addEventListener('click', closeMenu);
 
 document.querySelectorAll('#menu-lateral a').forEach(link => {
-  link.addEventListener('click', closeMenu);
+	link.addEventListener('click', closeMenu);
 });
 
 function closeMenu() {
-  sideMenu.classList.remove('open');
-  overlay.classList.remove('show');
-  document.body.classList.remove('menu-open');
+	sideMenu.classList.remove('open');
+	overlay.classList.remove('show');
+	document.body.classList.remove('menu-open');
 }
